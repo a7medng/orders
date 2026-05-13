c7692315c77b7ca5e28df06cd5630a12a932f3b4
a7medng <42158301+a7medng@users.noreply.github.com>
2026-05-13 17:34:00 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: 'Homiy System',
+  description: 'Order Management System',
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body>{children}</body>
+    </html>
+  )
+}
